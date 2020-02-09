7cebde57bc0d5ea3463b83471b38660bd0a5cb38
DentVega <dentvega6@gmail.com>
2020-02-09 14:57:59 -0400

    Preparando el cambio de direccion

--- src/app.rb
+++ src/app.rb
@@ -1,27 +1,32 @@
-# bundle exec ruby src/app.rb
-# puts "Hello"
 require_relative "view/ruby2d"
 require_relative "model/state"
 require_relative "actions/actions"
 
 class App
-
   def initialize
     @state = Model::initial_state
   end
 
   def start
-    view = View::Ruby2dView.new
-    Thread.new {init_timer(view)}
-    view.start(@state)
+    @view = View::Ruby2dView.new(self)
+    Thread.new { init_timer }
+    @view.start(@state)
   end
 
-  def init_timer(view)
-    loop do    
+  def init_timer
+    loop do
       @state = Actions::move_snake(@state)
-      view.render(@state)
-      # trigger movement
+      @view.render(@state)
       sleep 0.5
+    end
+  end
+
+  def send_action(action, params)
+    # :change_direction, Model::Direction::UP
+    new_state = Actions.send(action, @state, params)
+    if new_state.hash != @state
+      @state = new_state
+      @view.render(@state)
     end
   end
 end
--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -1,53 +1,98 @@
-require "ruby2d"
-require_relative "../model/state"
+module Actions
+  def self.move_snake(state)
+    next_direction = state.curr_direction
+    next_position = calc_next_position(state)
+    # verificar que la siguiente cassilla sea valida
+    if position_is_food?(state, next_position)
+      grow_snake_to(state, next_position)
+    elsif position_is_valid?(state, next_position)
+      move_snake_to(state, next_position)
+    else
+      end_game(state)
+    end
+  end
 
-module View
-  class Ruby2dView
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.curr_direction = direction
+    else
+      puts "Invalid direction"
+    end
+    state
+  end
 
-    def initialize
-      @pixel_size = 50
+  private
+
+  def self.position_is_food?(state, next_position)
+    state.food.row == next_position.row && state.food.col == next_position.col
+  end
+
+  def self.grow_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions
+    state.snake.positions = new_positions
+    state
+  end
+
+  def self.calc_next_position(state)
+    curr_position = state.snake.positions.first
+    case state.curr_direction
+    when Model::Direction::UP
+      # decrementar fila
+      return Model::Coord.new(
+          curr_position.row - 1, 
+          curr_position.col)
+    when Model::Direction::RIGHT
+      # incrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col + 1)
+    when Model::Direction::DOWN
+      # incrementar fila
+      return Model::Coord.new(
+          curr_position.row + 1,
+          curr_position.col)
+    when Model::Direction::LEFT
+      # decrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col - 1)
+    end
+  end
+
+  def self.position_is_valid?(state, position)
+    # verificar q este en la grilla
+    is_invalid = ((position.row >= state.grid.rows ||
+      position.row < 0) || 
+      (position.col >= state.grid.cols ||
+      position.col < 0))
+    return false if is_invalid
+    # verificar q no este superponiendo a la serpiente
+    return !(state.snake.positions.include? position)
+  end
+
+  def self.move_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions[0...-1]
+    state.snake.positions = new_positions
+    state
+  end
+
+  def self.end_game(state)
+    state.game_finished = true
+    state
+  end
+
+  def self.next_direction_is_valid?(state, direction)
+    case state.curr_direction
+    when Model::Direction::UP
+      return true if direction != Model::Direction::DOWN
+    when Model::Direction::DOWN
+      return true if direction != Model::Direction::UP
+    when Model::Direction::RIGHT
+      return true if direction != Model::Direction::LEFT
+    when Model::Direction::LEFT
+      return true if direction != Model::Direction::RIGHT
     end
 
-    def start(state)
-      extend Ruby2D::DSL
-      set(
-          title: "Snake", 
-          width: @pixel_size * state.grid.cols,
-          height: @pixel_size * state.grid.rows)
-      show
-    end
-
-    def render(state)
-      render_food(state)
-      render_snake(state)
-    end
-
-    private
-
-    def render_food(state)
-      @food.remove if @food
-      extend Ruby2D::DSL
-      food = state.food
-      @food = Square.new(
-        x: food.col * @pixel_size,
-        y: food.row * @pixel_size,
-        size: @pixel_size,
-        color: 'yellow'
-      )
-    end
-
-    def render_snake(state)
-      @snake_positions.each(&:remove) if @snake_positions
-      extend Ruby2D::DSL
-      snake = state.snake
-      @snake_positions = snake.positions.map do |pos|
-        Square.new(
-          x: pos.col * @pixel_size,
-          y: pos.row * @pixel_size,
-          size: @pixel_size,
-          color: 'green'
-        )
-      end
-    end
+    return false
   end
 end
